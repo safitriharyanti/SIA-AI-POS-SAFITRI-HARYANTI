bf9ae2c5e3a64555f959355f95947ebb705471be
Safitri Haryanti <safitriharyanti086@gmail.com>
2026-04-14 22:42:55 +0700

    Delete script.js

--- script.js
+++
@@ -1,7 +0,0 @@
-function prosesData() {
-  const input = document.getElementById("inputData").value;
-  const output = document.getElementById("output");
-
-  // contoh proses sederhana
-  output.innerText = "Hasil: " + input.toUpperCase();
-}
